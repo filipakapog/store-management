c5fdd24fdf207784dfa42927e6fb4ed176a0a59c
fghimpet <filip.ghimpeteanu@gmail.com>
2024-02-25 21:03:12 +0200

    Fail only the integration tests

--- src/test/java/org/filip/storemanagement/StoreManagementApplicationTests.java
+++ src/test/java/org/filip/storemanagement/StoreManagementApplicationTests.java
@@ -3,14 +3,12 @@ package org.filip.storemanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest
 class StoreManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
-		fail();
+
 	}
 
 }
