243107614ef13a31217c6f464b1f7ed0f8158050
fghimpet <filip.ghimpeteanu@gmail.com>
2024-02-25 20:37:04 +0200

    Fail intentionally unit and integration tests

--- src/test/java/org/filip/storemanagement/StoreManagementApplicationTests.java
+++ src/test/java/org/filip/storemanagement/StoreManagementApplicationTests.java
@@ -3,11 +3,14 @@ package org.filip.storemanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class StoreManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail();
 	}
 
 }
